880175c863c1cf1010a5d7194d09ce3c220c42e8
David Saper <davidsaper2@gmail.com>
2018-11-27 21:25:29 +0200

    added weeks

--- src/Home/PowerRankings/PowerRankings.js
+++ src/Home/PowerRankings/PowerRankings.js
@@ -7,13 +7,21 @@ import Team from "./Team";
 const TabPane = Tabs.TabPane;
 
 class PowerRankings extends PureComponent {
+
+	getWeekNum = (idx) => {
+		if (idx === 4) {
+			return 6
+		}
+		return idx + 1;
+
+	}
 	render() {
 		return (
 			<div className="power-rankings-container">
 				<img src="/power-rankings-logo-cut.png" alt="power-rankings" className="power-rankings-logo"/>
 				<Tabs defaultActiveKey={(POWER_RANKINGS_BY_WEEK.length).toString()}>
 					{POWER_RANKINGS_BY_WEEK.map((powerRankings, index) => {
-						return <TabPane tab={`Week ${index + 1}`} key={(index + 1).toString()}>
+						return <TabPane tab={`Week ${this.getWeekNum(index)}`} key={(index + 1).toString()}>
 							<div className="power-rankings-body">
 								{powerRankings.sort((team1, team2) => team2.rank - team1.rank).map(team =>
 									<Team key={team.name} {...team}/>
